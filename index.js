435b76e78403eb2e6c2b901d5ed01ab5bc2e832d
Daniel Lauzon <daniel.lauzon@irdeto.com>
2017-08-01 04:44:26 -0400

    Extract stamp from msg.seqno

--- index.js
+++ index.js
@@ -6,6 +6,24 @@ var ipfsAPI = require('ipfs-api')
 // or connect with multiaddr
 var ipfs = ipfsAPI('/ip4/127.0.0.1/tcp/5001')
 
+const start = +new Date()
+function int64NanosToDate (seqno) { // seqno is a buffer of 8 bytes
+  // generated here: https://github.com/libp2p/go-floodsub/blob/master/floodsub.go#L395
+  // binary.BigEndian.PutUint64(seqno, uint64(time.Now().UnixNano()))
+  // playground https://play.golang.org/p/90U-u-7sZF
+
+  // const bigInt = (msg.seqno.readUInt32BE(0) << 32) + msg.seqno.readUInt32BE(4)
+  const i0 = (seqno.readUInt32BE(0))
+  const i1 = (seqno.readUInt32BE(4))
+  // going from nanos to millis -> /1e6
+  const mult = (1 << 28) / (1e6 >> 4) // == (1<<32)/1e6 which you cannot do in JS!
+  const millis = i0 * mult + i1 / 1e6
+  // console.log('i0', i0, 'mult', mult, 'i0<<32 / 1e6 :', i0 * mult)
+  // console.log('i1', i1, 'i1 / 1e6 :', i1, i1 / 1e6)
+  // console.log('millis   :', millis)
+  // console.log('stamp    :', new Date(millis))
+  return new Date(millis)
+}
 async function main () {
   console.log('api', Object.keys(ipfs.pubsub))
 
@@ -15,20 +33,22 @@ async function main () {
 
   const receiveMsg = (msg) => {
     const rest = {
-      from: msg.from,
       // data: msg.data.toString(),
-      seqno: msg.seqno.toString('hex'),
+      stamp: int64NanosToDate(msg.seqno),
+      from: msg.from,
+      seqno: msg.seqno.toString('hex'), // binary.BigEndian.PutUint64(seqno, uint64(time.Now().UnixNano()))
       topicCIDs: msg.topicCIDs
     }
-    console.log('msg', msg.data.toString().trim(), JSON.stringify(rest))
+    const elapsed = Math.floor((+new Date() - start) / 1000)
+    console.log(`msg(+${elapsed}s)`, msg.data.toString().trim(), JSON.stringify(rest))
   }
   ipfs.pubsub.subscribe(topic, receiveMsg)
 
   setInterval(() => {
     ipfs.pubsub.publish(topic, Buffer.from(`hello@${new Date().toISOString()}\n`))
-    .catch(err => {
-      console.log('pub:err', err)
-    })
-  }, 1000)
+      .catch(err => {
+        console.log('pub:err', err)
+      })
+  }, 3000)
 }
 main()
